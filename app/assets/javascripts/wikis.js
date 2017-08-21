1a8c10da0047bb98222968e130cc7c51e5c209ca
Ben McKibben <mckibben@att.net>
2017-08-21 18:59:05 -0400

    menu collapsing

--- app/assets/javascripts/wikis.js
+++ app/assets/javascripts/wikis.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
-$(".sidebar-toggle").click(function() {
+	$(".sidebar-toggle").click(function() {
 
-$("#sidebar").toggleClass("collapsed");
-$("#content").toggleClass("col-md-12 col-md-10");
+		$("#sidebar").toggleClass("collapsed");
+		$("#content").toggleClass("col-md-12 col-md-10");
+		$("#content").toggleClass("fa-chevron-right fa-chevron-left");
 
-});
+	});
 
 }) //document.ready
