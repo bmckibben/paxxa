d55c1e8d2ede1353067d53bbdc362884f149d584
bbmckibb <bbmckibb@iu.edu>
2017-08-23 08:44:04 -0400

    began to modify wiki.js to include function for action icon click events. No yet tested.

--- app/assets/javascripts/wikis.js
+++ app/assets/javascripts/wikis.js
@@ -8,33 +8,33 @@ $(document).ready(function() {
 
 	});
 
-	$("#new-wiki").on("click", function() {
-		//checki if a new form is already open
-		if ($("#0").length != 0) {
-			alert("You must save or cancel new wiki before you can crate a new one.");
-			return;
-		}
-		formHTML = getForm(0);
-		insertHTML = '<div class="panel panel-default wiki-panel" id="0" >'+formHTML+'</div>'
-		$(".wiki-panel:first").before(insertHTML);
+	$("#wiki-new").on("click", function() {
 
+		if (checkExisting()) { return };		
+		InsertAtTop(getForm(0));
+		bindActionIcons($(this).data("id"));
 		// submit handler
-
 		//if save is successful replace content with new _display, update div id
-
-		$("#wiki-cancel").on("click", function() {
-			if(confirm("Are you sure you want to close this without saving?")){
-				$("#0").remove();
-			}	
-		});	
-
 	});
 
 	$(".wiki-edit").on("click", function() {
+
 		alert("you clicked clicked to edit wiki " + $(this).data("id"));
+		if (checkExisting()) { return };
+		editINPlace(getForm($(this).data("id")));
+		bindActionIcons($(this).data("id"));
+		// submit handler
+		//if save is successful replace content with new _display, update div id
 	});	
 
-
+	$("#wiki-here").on("click", function() {
+		
+		if (checkExisting()) { return };
+		InsertAfter(getForm($(this).data("id")));
+		bindActionIcons($(this).data("id"));
+		// submit handler
+		//if save is successful replace content with new _display, update div id
+	});
 
 }) //document.ready
 
@@ -54,3 +54,40 @@ var form;
     .fail(function(){ alert("Failed to retreive form.")});
     return form;
 }; //getForm
+
+function insertAtTop(formHTML){
+
+	insertHTML = '<div class="panel panel-default wiki-panel" id="0" >'+formHTML+'</div>'
+	$(".wiki-panel:first").before(insertHTML);()
+
+};	
+
+function insertAfter(formHTML, divID){
+
+	insertHTML = '<div class="panel panel-default wiki-panel" id="0" >'+formHTML+'</div>'
+	$("#"+divID).after(insertHTML);
+
+};
+
+function editInPlace(formHTML, divID){
+
+	insertHTML = '<div class="panel panel-default wiki-panel" id="'+divID+'" >'+formHTML+'</div>'
+	$("#"+divID).replaceWith(insertHTML);
+
+};
+
+function checkExisting(){
+	if ($("#0").length != 0) {
+		alert("You must save or cancel any new wiki before you can crate a new one.");
+		return true;
+	};
+	return false;
+};
+
+function bindActionIcons(divID) {
+	$("#wiki-cancel").on("click", function() {
+		if(confirm("Are you sure you want to close this without saving?")){
+			$("#0").remove();
+		}	
+	});	
+}
